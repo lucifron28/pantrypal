a0d1d7e18afc97eb4372920fcd99d202845c7ed5
ron <lucifron28@gmail.com>
2025-08-30 18:20:26 +0800

    Register UserPreferencesAdapter and open user_preferences box in main function

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:pantrypal/models/ingredient.dart';
 import 'package:pantrypal/models/recipe.dart';
 import 'package:pantrypal/models/pantry.dart';
+import 'package:pantrypal/models/user_preferences.dart';
 import 'package:pantrypal/app.dart';
 
 void main() async {
@@ -12,11 +13,13 @@ void main() async {
 
   Hive.registerAdapter(IngredientAdapter());
   Hive.registerAdapter(RecipeAdapter());
+  Hive.registerAdapter(PantryAdapter());
   
 
   await Hive.openBox<Ingredient>('ingredients');
   await Hive.openBox<Recipe>('recipes');
   await Hive.openBox<Pantry>('pantries');
+  await Hive.openBox<UserPreferences>('user_preferences');
 
   runApp(const PantryPalApp());
 }
