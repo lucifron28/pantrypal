42af3ec8bd58941c89c090b4d5af183da7a776e4
ron <lucifron28@gmail.com>
2025-08-30 19:06:39 +0800

    Refactor HomeScreen to use StatefulWidget and implement bottom navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,52 @@
-// Home Screen
 import 'package:flutter/material.dart';
+import 'package:pantrypal/screens/pantry_screen.dart';
+import 'package:pantrypal/screens/recipe_suggestions_screen.dart';
+import 'package:pantrypal/screens/paywall_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _screens = <Widget>[
+    PantryScreen(),
+    RecipeSuggestionsScreen(),
+    PaywallScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('PantryPal Home')),
-      body: Center(child: Text('Welcome to PantryPal!')),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.kitchen),
+            label: 'Pantry',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt),
+            label: 'Recipes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.lock),
+            label: 'Paywall',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
