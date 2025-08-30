05527f91c96e15a000619a4a236737d96025bf60
ron <lucifron28@gmail.com>
2025-08-30 17:26:57 +0800

    Refactor main function to initialize Hive and register IngredientAdapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,12 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'screens/pantry_screen.dart';
-import 'screens/recipe_suggestions_screen.dart';
-import 'screens/paywall_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'models/ingredient.dart';
+import 'app.dart';
 
-void main() {
-  runApp(PantryPalApp());
-}
-
-class PantryPalApp extends StatelessWidget {
-  const PantryPalApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PantryPal',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/pantry': (context) => PantryScreen(),
-        '/recipes': (context) => RecipeSuggestionsScreen(),
-        '/paywall': (context) => PaywallScreen(),
-      },
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(IngredientAdapter());
+  await Hive.openBox<Ingredient>('ingredients');
+  runApp(const PantryPalApp());
 }
