c202dbf28c60aeebd3f7520b9c013f0b20e7cefc
ron <lucifron28@gmail.com>
2025-08-30 17:59:47 +0800

    Register RecipeAdapter and open recipes box in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'models/ingredient.dart';
-import 'app.dart';
+import 'package:pantrypal/models/ingredient.dart';
+import 'package:pantrypal/models/recipe.dart';
+import 'package:pantrypal/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Hive.initFlutter();
+
   Hive.registerAdapter(IngredientAdapter());
+  Hive.registerAdapter(RecipeAdapter());
+
   await Hive.openBox<Ingredient>('ingredients');
+  await Hive.openBox<Recipe>('recipes');
+
   runApp(const PantryPalApp());
 }
