c20af3fbdafb1fcde3f04aaaa1a747b896bac52f
ron <lucifron28@gmail.com>
2025-08-30 17:11:24 +0800

    Add new factory constructor for Ingredient to generate unique IDs and improve JSON serialization

--- lib/models/ingredient.dart
+++ lib/models/ingredient.dart
@@ -1,9 +1,42 @@
+import 'package:uuid/uuid.dart';
+
 class Ingredient {
+  final String id;
   final String name;
   final String quantity;
 
   Ingredient({
+    required this.id,
     required this.name,
     required this.quantity,
   });
+
+  // New Ingredient Constructor
+  factory Ingredient.newIngredient({
+    required String name,
+    required String quantity,
+  }) {
+    final uuid = Uuid();
+    return Ingredient(
+      id: uuid.v4(),
+      name: name,
+      quantity: quantity,
+    );
+  }
+
+  // JSON Serializer
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'quantity': quantity,
+      };
+
+  // JSON Deserializer
+  factory Ingredient.fromJson(Map<String, dynamic> json) {
+    return Ingredient(
+      id: json['id'],
+      name: json['name'],
+      quantity: json['quantity'],
+    );
+  }
 }
