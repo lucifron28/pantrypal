07e8f246a46d2fa39b35f0cdb18b020f6ccc5755
ron <lucifron28@gmail.com>
2025-08-30 16:59:15 +0800

    quick fix

--- lib/screens/paywall_screen.dart
+++ lib/screens/paywall_screen.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 
 class PaywallScreen extends StatelessWidget {
+  const PaywallScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
