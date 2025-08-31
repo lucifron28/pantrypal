af10b97be761d973713ef589f3c354d329d3c511
ron <lucifron28@gmail.com>
2025-08-31 16:25:21 +0800

    Refactor PantryList: convert to ConsumerWidget and implement ingredient detail dialog

--- lib/widgets/pantry_list.dart
+++ lib/widgets/pantry_list.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:pantrypal/models/pantry.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pantrypal/providers/pantry_provider.dart';
+import 'package:pantrypal/widgets/ingredient_detail_dialog.dart';
 
-class PantryList extends StatelessWidget {
+class PantryList extends ConsumerWidget {
   final Pantry pantry;
   const PantryList({super.key, required this.pantry});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colorScheme = Theme.of(context).colorScheme;
     if (pantry.ingredients.isEmpty) {
       return Center(
@@ -28,8 +31,19 @@ class PantryList extends StatelessWidget {
           trailing: ingredient.expiryDate != null
               ? Text('Exp: ${ingredient.expiryDate!.toLocal().toString().split(" ")[0]}', style: TextStyle(color: colorScheme.error))
               : null,
-          onTap: () {
-            // TODO: Show ingredient detail modal
+          onTap: () async {
+            await showDialog(
+              context: context,
+              builder: (context) => IngredientDetailDialog(
+                ingredient: ingredient,
+                onSave: (updated) {
+                  ref.read(pantryProvider.notifier).updateIngredient(updated);
+                },
+                onDelete: () {
+                  ref.read(pantryProvider.notifier).deleteIngredient(ingredient.id);
+                },
+              ),
+            );
           },
         );
       },
