5ac3093bc54d2bea9e67f857e54504a19975de2a
ron <lucifron28@gmail.com>
2025-08-31 13:02:31 +0800

    Remove unused Flutter import from FirebaseService

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -2,7 +2,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 
 class FirebaseService {
   static Future<void> initialize() async {
